fbb1d98288b9ccf909f85803aba8e23112af8df7
Bruno Murua <100427363+SrZeroRequiem@users.noreply.github.com>
2023-12-04 16:54:00 +0000

    Final commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
 /* eslint-disable */
-import "bootstrap";
+//import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log(randomExcuse());
+  document.getElementById("excuseButton").onclick = function() {
+    document.getElementById("excuse").innerHTML = randomExcuse();
+  };
 };
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+let words = [who, action, what, when];
+function randomExcuse() {
+  let excuses = "";
+  for (let i = 0; i < words.length; i++) {
+    excuses = excuses + " " + words[i][randomNumber(words[i].length)];
+  }
+  return excuses;
+}
+function randomNumber(lenght) {
+  let number = Math.floor(Math.random() * lenght);
+  return number;
+}
